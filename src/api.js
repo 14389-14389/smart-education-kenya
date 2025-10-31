ca5d2f8f5778772e0821b12b72d7fb96b9349196
14389-14389Devkevinmuli <kevinmuli047@gmail.com>
2025-10-31 15:46:46 +0300

    Update API baseURL to deployed backend URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000", // Flask backend (change to deployed URL later)
+  baseURL: "https://smart-education-backend.onrender.com", // Deployed Flask backend
 });
 
 // Example function to test API
